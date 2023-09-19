401ee398fe7737607dcae37c3b481079d04808e9
Behruz Ahmedov <AhmedovB@StPaulsSchool.org.uk>
2023-09-19 19:35:32 +0100

    Add project files.

---
+++ Decimal-to-Floating-Point-Binary-Converter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Decimal_to_Floating_Point_Binary_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
